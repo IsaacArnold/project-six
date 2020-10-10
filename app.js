23e15e3ed5b0f2c4a184a24cea8171674c85c36b
Isaac Arnold <isaacarnold@icloud.com>
2020-10-10 13:06:34 +1300

    Created phrases array

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const letters = document.querySelector('#qwerty');
-const phraseDiv = document.querySelector('#phrase');
+const phrase = document.querySelector('#phrase');
 const missed = 0;
 const startGameButton = document.querySelector('.btn__reset');
 
+// Removes start screen overlay when Start Game button is clicked
 startGameButton.addEventListener('click', () => {
     const overlayDiv = document.querySelector('#overlay');
     overlayDiv.style.display = 'none';
 });
+
+// Array of phrases
+const phrases = [
+    'hello world',
+    'the sun is hot',
+    'elephants are big',
+    'i love coding',
+    'javascript is hard'
+];
